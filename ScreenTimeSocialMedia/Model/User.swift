994c55894eedbba89c38d3bb6480d835e615b902
Andrew Kuznetsov <andrew.kuz137@gmail.com>
2023-09-06 20:36:45 -0700

    Reorganizing all files

---
+++ ScreenTimeSocialMedia/Model/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  ScreenTimeSocialMedia
+//
+//  Created by Andrew Kuznetsov on 9/6/23.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    let username: String
+    let email: String
+    let password: String
+    
+    
+}
+
